1d19d49853da43ffa413306ad4dc9412f8038819
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-30 18:59:35 +0600

    Update start.py

--- start.py
+++ start.py
@@ -15,13 +15,7 @@ async def handler(event):
 @client.on(events.NewMessage(pattern='/help'))
 async def handler(event):
     chat = await event.get_chat()
-    await client.send_message(chat,"""Hi, this is **Zee5** & **MX Player** Video Transloader Bot. You can use me for making Streamable Link of any Zee5 or MX Player Video. Just copy Video Link from Zee5 or MX Player and send it to me I will Transload it and send it to you. \n\n⭕️ MX Player Example: https://www.mxplayer.in/movie/039664d4d85c603cfb5a6cd66b9e29ec \n⭕️ Zee5 Example: https://www.zee5.com/movies/details/courier-boy-kalyan-2015-hindi-drama/0-0-courierboykalyan \nFormat should be like this, else bot will not work. And no DRM Protected or Premuim Videos Please. \n\nFor more help ask in @linux_repo""")
-
-#@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
-#async def help_user(bot, update):
-#    # logger.info(update)
-#    TRChatBase(update.from_user.id, update.text, "/help")
-#    await bot.send_message
+    await client.send_message(chat,"""Hi, this is **Zee5** & **MX Player** Video Transloader Bot. You can use me for making Streamable Link of any Zee5 or MX Player Video. Just copy Video Link from Zee5 or MX Player and send it to me I will Transload it and send it to you. \n\n⭕️ **MX Player Example:** https://www.mxplayer.in/movie/039664d4d85c603cfb5a6cd66b9e29ec \n⭕️ **Zee5 Example:** https://www.zee5.com/movies/details/courier-boy-kalyan-2015-hindi-drama/0-0-courierboykalyan \n\nFormat should be like this, else bot will not work. And no DRM Protected or Premuim Videos Please. \n\nFor more help ask in @linux_repo""")
 
 @client.on(events.NewMessage(pattern='(?i)https://www.zee5.com'))
 
@@ -39,8 +33,8 @@ async def handler(event):
     r1 = requests.get(urls.search_api_endpoint + w,headers=headers, params={"translation":"en", "country":"IN"}).json()
     g1 = (r1["hls"][0].replace("drm", "hls") + req1["video_token"])
    # await client.send_file(chat,r1["image_url"],caption = r1["title"])
-    markup = client.build_reply_markup(Button.url("stream",urls.stream_baseurl+g1))
-    await client.send_message(chat, "Zee5 Link Transloaded! \n "+" Video Title: "+r1["title"]+" | Video Description: "+r1["description"],file=r1["image_url"], buttons=markup)
+    markup = client.build_reply_markup(Button.url("Transloaded Link",urls.stream_baseurl+g1))
+    await client.send_message(chat, "Zee5 Link Transloaded! \n\n"+"**Video Title:** "+r1["title"]+" \n**Video Description: __"+r1["description"] "\n__",file=r1["image_url"], buttons=markup)
             
             #rgx = w
    # await client.send_message(chat, g1)
@@ -52,7 +46,7 @@ async def handler(event):
     video_d = "https://llvod.mxplay.com/"
     A =requests.get("https://api.mxplay.com/v1/web/detail/video?type=movie&id="+link+"&platform=com.mxplay.desktop&device-density=2&userid=30bb09af-733a-413b-b8b7-b10348ec2b3d&platform=com.mxplay.mobile&content-languages=hi,en,ta").json()
     chat = await event.get_chat()
-    markup = client.build_reply_markup(Button.url("stream",video_d+A["stream"]['hls']['high']))
+    markup = client.build_reply_markup(Button.url("Transloaded Link",video_d+A["Transloaded Link"]['hls']['high']))
     await client.send_message(chat,"Title: "+A["title"],buttons=markup)
     print(A)
     print(link)
